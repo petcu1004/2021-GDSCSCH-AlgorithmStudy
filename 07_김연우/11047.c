975d651738ceaf62b22443279fa2c65d666d330f
YeonWooKim <67813381+Hello-slkdfjsej@users.noreply.github.com>
2021-11-04 09:37:26 +0900

    Create 11047.c

---
+++ 07_김연우/11047.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int Loop_Num, K, Count = 0;
+	scanf_s("%d %d", &Loop_Num, &K);
+	int* N = malloc(sizeof(int) * Loop_Num);
+	for (int i = 0; i < Loop_Num; i++) {scanf_s("%d", &N[i]);}
+
+	for (int i = Loop_Num; i > 0; i--) { 
+		if (K / N[i] > 0) {
+			Count += K / N[i];
+			K = K % N[i];
+		}
+	}
+	printf("%d", Count);
+
+	free(N);
+	return 0;
+}
