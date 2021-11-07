d8c6c7e70ffac88bff44a4361f24f8fb06d67207
kimsubingithub <90540672+kimsubingithub@users.noreply.github.com>
2021-11-07 23:59:41 +0900

    Add files via upload

---
+++ 24_김수빈/11650.c
@@ -0,0 +1,37 @@
+// 작성일: 2021년 11월 7일
+// 작성자: 김수빈
+// 프로그램명: 좌표 정렬하기
+#include <stdio.h>
+
+typedef struct {
+	int x, y;
+} Coordinate;
+
+void coor_sort(Coordinate* c, int n) {
+	Coordinate temp;
+	for (int i = 0; i < n; i++) {
+		for (int j = i; j < n; j++) {
+			if (((c[i].x == c[j].x) && (c[i].y > c[j].y)) || (c[i].x > c[j].x)) {
+				temp = c[i];
+				c[i] = c[j];
+				c[j] = temp;
+			}
+		}
+	}
+}
+
+int main() {
+	int number;
+	Coordinate* coor;
+
+	scanf_s("%d", &number);
+	coor = (Coordinate*)malloc(sizeof(Coordinate) * number);
+	for (int i = 0; i < number; i++)
+		scanf_s("%d %d", &coor[i].x, &coor[i].y);
+
+	coor_sort(coor, number);
+	for (int i = 0; i < number; i++)
+		printf("\n%d %d", coor[i].x, coor[i].y);
+	free(coor);
+	return 0;
+}
