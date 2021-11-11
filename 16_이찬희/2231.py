2e4d1b7e69d096b66f1a07baf28b5f4107b5d460
Chanhee_ <92356803+0430-lch@users.noreply.github.com>
2021-11-11 17:10:40 +0900

    Update 2231.py

--- 16_이찬희/2231.py
+++ 16_이찬희/2231.py
@@ -1,10 +1,12 @@
+#분해합
+
 n = int(input())
 
-for i in range(1, n+1):  
-    num = sum(map(int, str(i)))
-    num_sum = i + num  
+for i in range(1, n+1):  # map(function, iterable) : 함수, 반복가능한 자료형(리스트 , 튜플 등) / 반환값 : map 객체
+    num = sum(map(int, str(i)))  #num에는 각 자리 수들의 합이 저장된다. 
+    num_sum = i + num  # num_sum = 각 자리 수 합 + 숫자 = 분해합 
 
-    if num_sum == n:
+    if num_sum == n: 
         print(i)
         break
     if i == n: 
