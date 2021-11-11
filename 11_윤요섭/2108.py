0e507c504e0ab8a6eeeb85c40ca5e9ad4ab0202c
yoonyoseob <75351859+yoonyoseob@users.noreply.github.com>
2021-11-11 22:59:14 +0900

    2108 최빈값 스스로 만들었으나, 시간 복잡도에 막힘

--- 11_윤요섭/2108.py
+++ 11_윤요섭/2108.py
@@ -1,3 +1,4 @@
+from collections import Counter
 n = int(input())
 num_list = []
 for i in range(n):
@@ -6,25 +7,30 @@ for i in range(n):
 
 num_list = sorted(num_list)
 num_tuple = tuple(num_list)
-print()
+#산술평균
 print(round(sum(num_list) / n))
+#중앙값
 print(num_list[int(n / 2)])
 
 max_num = max(num_list)
 min_num = min(num_list)
-cnt_list = [0] * (max_num - min_num)
-for i in num_list:
-    cnt_list[i - 1] += 1
-print(cnt_list)
+#최빈값
+mode = num_list.count(min_num)
+mode_list = [mode]
+for i in range(len(num_list)):
+    mode_list = sorted(mode_list)
+    #지금 세고 있는 값의 양이 기존의 최빈값보다 같다면, 최빈값 리스트에 삽입
+    if num_list.count(num_list[i]) == mode:
+        mode_list.append(num_list[i])
+    #지금 세고 있는 값의 양이 기존의 최빈값보다 많다면, 최빈값 리스트를 초기화하고, 해당 값을 리스트에 넣은 다음 최빈값을 해당 값으로 바꿔준다
+    elif num_list.count(num_list[i]) > mode:
+        mode_list = []
+        mode_list.append(num_list[i])
+        mode = num_list.count(num_list[i])
 
-save = []
-for i in range(len(cnt_list)):
-    if cnt_list[i] == max(cnt_list):
-        save.append(i + 1)
-print(save)
-if len(save) > 1:
-    print(save[1])
+if len(mode_list) == 1:
+    print(mode_list[0])
 else:
-    print(save[0])
-
-print(num_list[-1] - num_list[0])
+    print(mode_list[1])
+#범위
+print(max_num - min_num)
