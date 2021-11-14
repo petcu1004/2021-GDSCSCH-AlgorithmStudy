45b400578a1d147ceb07050bc97b6960d2c17e3a
yoonyoseob <75351859+yoonyoseob@users.noreply.github.com>
2021-11-14 18:31:51 +0900

    오류

--- 11_윤요섭/2108.py
+++ 11_윤요섭/2108.py
@@ -1,8 +1,9 @@
 from collections import Counter
-n = int(input())
+import sys
+n = int(sys.stdin.readline())
 num_list = []
 for i in range(n):
-    a = int(input())
+    a = int(sys.stdin.readline())
     num_list.append(a)
 
 num_list = sorted(num_list)
@@ -14,23 +15,42 @@ print(num_list[int(n / 2)])
 
 max_num = max(num_list)
 min_num = min(num_list)
-#최빈값
-mode = num_list.count(min_num)
-mode_list = [mode]
+#최빈값 계산
+counter = Counter(num_list)
+mode = 0  #최빈값
+mode_num_1 = min_num  #최빈수중 첫번째로 작은 값
+mode_num_2 = 0  #최빈수중 두번째로 작은 값
 for i in range(len(num_list)):
-    mode_list = sorted(mode_list)
-    #지금 세고 있는 값의 양이 기존의 최빈값보다 같다면, 최빈값 리스트에 삽입
-    if num_list.count(num_list[i]) == mode:
-        mode_list.append(num_list[i])
-    #지금 세고 있는 값의 양이 기존의 최빈값보다 많다면, 최빈값 리스트를 초기화하고, 해당 값을 리스트에 넣은 다음 최빈값을 해당 값으로 바꿔준다
-    elif num_list.count(num_list[i]) > mode:
-        mode_list = []
-        mode_list.append(num_list[i])
-        mode = num_list.count(num_list[i])
+    current_num = num_list[i]
+    now_counter_num = counter[current_num]
+    #현재 카운터 값이 지금까지의 최빈값보다 클때
+    if now_counter_num > mode:
+        mode = now_counter_num
+        mode_num_1 = current_num
+        mode_num_2 = ''
+    #현재 카운터 값과 지금까지의 최빈값의 크기가 같을때
+    elif now_counter_num == mode:
+        #mode_num_1과 mode_num_2값이 모두 차있을때
+        if mode_num_2 != '':
+            #지금 값이 3개의 수중 가장 작을때
+            if mode_num_1 > current_num:
+                mode_num_2 = mode_num_1
+                mode_num_1 = current_num
+            #지금 값이 두번째로 작을때
+            if mode_num_1 < current_num and current_num < mode_num_2:
+                mode_num_2 = current_num
+        else:
+            #현재 저장된 값이 더 작을때
+            if mode_num_1 < current_num:
+                mode_num_2 = current_num
+            #현재 저장된 값보다 더 작을때
+            else:
+                mode_num_2 = mode_num_1
+                mode_num_1 = current_num
 
-if len(mode_list) == 1:
-    print(mode_list[0])
+if mode_num_2 == '':
+    print(mode_num_1)
 else:
-    print(mode_list[1])
+    print(mode_num_1)
 #범위
 print(max_num - min_num)
