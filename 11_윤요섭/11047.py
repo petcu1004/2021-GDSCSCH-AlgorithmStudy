d069a5e8cadfc0d38dc8d46d8aa2888d1f7f03ba
yoonyoseob <75351859+yoonyoseob@users.noreply.github.com>
2021-11-15 02:57:54 +0900

    3주차 밀린 문제

--- 11_윤요섭/11047.py
+++ 11_윤요섭/11047.py
@@ -1,12 +1,34 @@
 import sys
-n, k = map(int, sys.stdin.readline().split())
-coin_list = []
+n = int(sys.stdin.readline())
+stack_list = []
 for i in range(n):
-    coin = int(sys.stdin.readline())
-    coin_list.append(coin)
-a = 0
-for i in range(len(coin_list)):
-    if k % coin_list[-(i + 1)] < k:
-        a += int(k / coin_list[-(i + 1)])
-        k %= coin_list[-(i + 1)]
-print(a)
+    c = sys.stdin.readline()
+    if 'push' in c:
+        c, k = c.split(' ')
+        k = int(k)
+        stack_list.append(k)
+
+    elif 'pop' in c:
+        if len(stack_list) == 0:
+            print(-1)
+
+        else:
+            print(stack_list[-1])
+            del stack_list[-1]
+
+    elif 'size' in c:
+        print(len(stack_list))
+
+    elif 'empty' in c:
+        if len(stack_list) == 0:
+            print(1)
+
+        else:
+            print(0)
+
+    elif 'top' in c:
+        if len(stack_list) == 0:
+            print(-1)
+
+        else:
+            print(stack_list[-1])
