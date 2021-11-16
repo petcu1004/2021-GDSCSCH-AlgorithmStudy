8916b159193aab69c4f7d4b9daa08562515b843f
Chanhee_ <92356803+0430-lch@users.noreply.github.com>
2021-11-16 14:02:06 +0900

    Update 15552.py

--- 16_이찬희/15552.py
+++ 16_이찬희/15552.py
@@ -1,6 +1,8 @@
+#빠른 A + B
+
 import sys
 n = int(input())
 
 for i in range(n):
-    a, b = map(int, sys.stdin.readline().split())
+    a, b = map(int, sys.stdin.readline().split()) #정해진 개수의 정수를 한줄에 입력받을 때
     print(a + b)
