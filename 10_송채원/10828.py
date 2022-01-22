ff43de72f30c4ddd804a81007a55c0c7495a1ffd
Chaewon Song <81732426+chaewonS@users.noreply.github.com>
2022-01-23 01:02:12 +0900

    winter_3week

--- 10_송채원/10828.py
+++ 10_송채원/10828.py
@@ -1,26 +1,21 @@
 import sys
-n = int(sys.stdin.readline())
+input = sys.stdin.readline
 
+n = int(input())
 stack = []
+
 for i in range(n):
-    cmd = sys.stdin.readline().split()
-
-    if cmd[0] == 'push':
-        stack.append(cmd[1])
-    
-    elif cmd[0] == 'pop':
-        if len(stack)==0:
-            print(-1)
-        else:
-            print(stack.pop())
-
-    elif cmd[0] == 'size':
+    s = input().split()
+    if s[0] == 'push':
+        stack.append(int(s[1]))
+    elif s[0] == 'top':
+        if len(stack) > 0: print(stack[-1])
+        else: print(-1)
+    elif s[0] == 'pop':
+        if len(stack) > 0: print(stack.pop())
+        else: print(-1)
+    elif s[0] == 'size':
         print(len(stack))
-
-    elif cmd[0] == 'empty':
-        if len(stack)==0:
-            print(1)
-        else:
-            print(0)
-
-    
+    elif s[0] == 'empty':
+        if len(stack) > 0: print(0)
+        else: print(1)
