a1a156d7528d0e9a9215d22a20ff01fadeb60a13
Yoseob Yun <yosub1220@naver.com>
2022-02-08 00:46:29 +0900

    push_front insert 메소드

--- 11_윤요섭/10866.py
+++ 11_윤요섭/10866.py
@@ -1,12 +1,12 @@
 import sys
 n = int(input())
+deq = []
 for i in range(n):
-    deq = []
     c = sys.stdin.readline().split()
-    if c == 'push_back':
+    if c[0] == 'push_back':
         deq.append(c[1])
-    elif c == 'push_front':
-        deq.appendleft(c[1])
+    elif c[0] == 'push_front':
+        deq.insert(0, c[1])
     elif c[0] == 'pop_front':
         if len(deq) != 0:
             print(deq[0])
