8b4aedbba1d9ac541fc3a380ee378ef4c3e034b2
hyunjin-C <zn9852@naver.com>
2022-02-16 15:39:29 +0900

    10809.py

---
+++ 22_최현진/10809.py
@@ -0,0 +1,9 @@
+S = input()
+
+alpabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for i in alpabet:
+    if i in S:
+        print(S.find(i), end=' ')
+    else:
+        print(-1, end=' ')
